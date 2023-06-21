de6ce3acbaad8ba142cadcfdf66117ac659e5901
Rodrigo <rodrigomartins.agro@gmail.com>
2023-06-21 18:46:18 -0300

    feat: add inventory_control

--- src/services/inventory_control.py
+++ src/services/inventory_control.py
@@ -27,8 +27,15 @@ class InventoryMapping:
 
     # Req 5.1
     def check_recipe_availability(self, recipe: Recipe) -> bool:
-        pass
+        for key, value in recipe.items():
+            if key not in self.inventory or self.inventory[key] < value:
+                return False
+        return True
 
     # Req 5.2
     def consume_recipe(self, recipe: Recipe) -> None:
-        pass
+        if self.check_recipe_availability(recipe) is False:
+            raise ValueError
+        else:
+            for key, value in recipe.items():
+                self.inventory[key] -= value
